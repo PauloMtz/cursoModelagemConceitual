7c86f6cfcd83e9e90dde899f694dae5565db737d
PauloMtz <pramosmtz@gmail.com>
2018-08-05 20:46:25 -0300

    Criação do projeto para o curso de Modelagem Conceitual com UML

---
+++ src/main/java/com/modelagem/CursomodelagemudemyApplication.java
@@ -0,0 +1,12 @@
+package com.modelagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomodelagemudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomodelagemudemyApplication.class, args);
+	}
+}
