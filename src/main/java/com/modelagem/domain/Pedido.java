ced926c6b29d462a4a6cabf94a7c32b658287c8d
PauloMtz <pramosmtz@gmail.com>
2018-08-22 18:25:12 -0300

    Mapeamento de pedido e pagamento, do tipo um para um

--- src/main/java/com/modelagem/domain/Pedido.java
+++ src/main/java/com/modelagem/domain/Pedido.java
@@ -3,14 +3,25 @@ package com.modelagem.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Pedido implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private Date dataHora;
 	
 	// associação com pagamento
+	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
 	// associação com cliente
